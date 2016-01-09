1fee75f519dd8c96b35b7c01d6ab6f886ece0f8b
Lin CHEN <lin.chen.paris@gmail.com>
2016-01-09 15:08:27 +0100

    [EVOL] ameliorer text, ajoute detection lang navi

--- php/config.php
+++ php/config.php
@@ -3,11 +3,11 @@ $url_prefix = "/";
 
 // language
 //$lang_support = array("cn","cntr","fr","en");
-$lang_support = array("cn","fr");
+$lang_support = array("cn", "fr");
 $lang_support_txt = array("cn"=>"中","cntr"=>"漢","en"=>"EN","fr"=>"FR");
-$lang_navi = "cn"; // TODO
-$lang_sys = "cn"; // TODO
-$lang_client = "cn";
+$lang_navi = getNaviLang();
+$lang_sys = getSysLang();
+$lang_client = getClientLang();
 
 $html_support = array("html", "php");
 $source_support = array("js","css","htc");
@@ -21,4 +21,36 @@ $contentType = "html";
 $contentName = "index";
 $contentSuffix = "html";
 $pagePath = "/translation/index";
+
+function getNaviLang(){
+	$lang = locale_accept_from_http($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+	if($lang==null){
+		return getDefalutLang();
+	}else if(substr($lang, 0, 2)=="fr"){
+		return "fr";
+	}else if(substr($lang, 0, 2)=="en"){
+//		return "en"; // TODO
+		return "fr";
+	}else if(substr($lang, 0, 2)=="zh"){
+		if($lang=="zh-cn" || $lang=="zh-sg"){
+			return "cn";
+		}else{
+//			return "cntr"; // TODO
+			return "cn";
+		}
+	}
+}
+
+function getSysLang(){ // TODO
+	return getNaviLang();
+}
+
+function getDefalutLang(){
+	return "fr";
+}
+
+function getClientLang(){
+	return getNaviLang();
+}
+
 ?>
