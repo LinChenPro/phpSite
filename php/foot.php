98bab11d1183bf2db54728ffd2bc4f424c085418
Lin CHEN <lin.chen.paris@gmail.com>
2016-03-22 13:36:44 +0100

    [EVOL] upload file step 3 version cn

--- php/foot.php
+++ php/foot.php
@@ -2,39 +2,42 @@
 
 	<div id="foot-area">
 		<span class="tft">
-			<span id="foot_name_1" class="tred">
-			睿思国际
-			</span>
+			<span class="inl_blc">
+				<span id="foot_name_2">
+				睿思国际
+				</span>
 
-			<span id="foot_name_2">
-			IRISO
-			</span>
+				<span id="foot_name_2">
+				IRISO
+				</span>
 
-			<span id="foot_name_3">
-			Service International
-			</span>
+				<span id="foot_name_3">
+				Service International
+				</span>
 
-			<span id="foot_name_4">
-			S.A.S.U.
-			</span>
-
-			<span id="foot_copyright">
-			- <span class="bd">Copyright © 2015</span>
-			</span>
-<br>
-			<span id="foot_address">
-			<span class="bd">Addr :</span> 102 Avenue des Champs-Élysées - 75008 Paris
+				<span id="foot_name_4">
+				S.A.S.U.
+				</span>
 			</span>
 			<wbr>
-			<span id="foot_tel">
-			<span class="bd">Tel :</span> (+33) 1 79 06 03 75
+			<span id="foot_copyright">
+				<span class="bd">Copyright © 2016</span>
 			</span>
-			<span id="foot_fax">
-			<span class="bd">Fax :</span> (+33) 9 81 40 96 43
+			<wbr>
+			<span id="foot_address">
+				<span class="bd">Addr :</span> 102 Avenue des Champs-Élysées - 75008 Paris
+			</span>
+			<wbr>
+			<span class="inl_blc">
+				<span id="foot_tel">
+					<span class="bd">Tel :</span> (+33) 1 79 06 03 75
+				<span id="foot_fax">
+					<span class="bd">Fax :</span> (+33) 9 81 40 96 43
+				</span>
 			</span>
 			<wbr>
 			<span id="foot_contact">
-			<span class="bd">Email :</span> contact@iriso-service.com
+				<span class="bd">Email :</span> contact@iriso-service.com
 			</span>
 		</span>
 	</div>
